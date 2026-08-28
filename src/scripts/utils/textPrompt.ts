b54727d62112db0351e874a352a8feac4e0ea063
Josh Cunningham <josh@joshcanhelp.com>
2026-08-28 16:16:22 -0700

    fix: prompt line

--- src/scripts/utils/textPrompt.ts
+++ src/scripts/utils/textPrompt.ts
@@ -5,7 +5,7 @@ export async function textPrompt(prompt: string): Promise<string> {
   const rl = createInterface({ input: process.stdin, output: process.stdout });
 
   return new Promise((resolve) => {
-    rl.question(`\n${prompt}: `, (answer) => {
+    rl.question(`${prompt}: `, (answer) => {
       rl.close();
       resolve(answer.trim());
     });
