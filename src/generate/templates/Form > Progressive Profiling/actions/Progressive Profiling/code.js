b9f6c7fd1d9280c1b5b1247b1865b3228b78457e
Josh Cunningham <josh@joshcanhelp.com>
2026-09-09 12:15:15 -0700

    feat: progressive profiling form

---
+++ src/generate/templates/Form > Progressive Profiling/actions/Progressive Profiling/code.js
@@ -0,0 +1,23 @@
+exports.onExecutePostLogin = async (event, api) => {
+  if (event.request.query.action !== 'profile_form') {
+    return;
+  }
+  
+  if (event.user.user_metadata?.address_line_1) {
+    return;
+  }
+  
+  api.prompt.render('ap_dcLAcEMXfi1xfHZ5qsfoct');
+};
+
+exports.onContinuePostLogin = async (event, api) => {
+  const f = event.prompt.fields;
+  api.user.setUserMetadata('address', {
+    line1: f.address_line_1,
+    line2: f.address_line_2 || '',
+    zip: f.zip,
+    city: f.city,
+    state: f.state,
+    country: f.country
+  });
+};
