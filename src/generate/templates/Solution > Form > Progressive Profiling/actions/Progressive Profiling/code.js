03776d2374a5a1cc38d731765a84192abb699920
Josh Cunningham <josh@joshcanhelp.com>
2026-09-11 13:26:12 -0700

    refactor: renaming/moving

--- src/generate/templates/Solution > Form > Progressive Profiling/actions/Progressive Profiling/code.js
+++ src/generate/templates/Solution > Form > Progressive Profiling/actions/Progressive Profiling/code.js
@@ -1,23 +1,23 @@
 exports.onExecutePostLogin = async (event, api) => {
-  if (event.request.query.action !== 'profile_form') {
+  if (event.request.query.action !== "profile_form") {
     return;
   }
-  
+
   if (event.user.user_metadata?.address_line_1) {
     return;
   }
-  
-  api.prompt.render('ap_dcLAcEMXfi1xfHZ5qsfoct');
+
+  api.prompt.render("ap_dcLAcEMXfi1xfHZ5qsfoct");
 };
 
 exports.onContinuePostLogin = async (event, api) => {
   const f = event.prompt.fields;
-  api.user.setUserMetadata('address', {
+  api.user.setUserMetadata("address", {
     line1: f.address_line_1,
-    line2: f.address_line_2 || '',
+    line2: f.address_line_2 || "",
     zip: f.zip,
     city: f.city,
     state: f.state,
-    country: f.country
+    country: f.country,
   });
 };
